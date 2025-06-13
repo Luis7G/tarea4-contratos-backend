27fb71a870b5df68a9f47c302683d8525b96aa6b
Joel Guingla <joel.guingla7@gmail.com>
2025-06-13 09:38:53 -0500

    fix: pdf service interface

--- ContratosPdfApi/Services/IPdfService.cs
+++ ContratosPdfApi/Services/IPdfService.cs
@@ -3,7 +3,7 @@ using ContratosPdfApi.Models;
 namespace ContratosPdfApi.Services
 {
     public interface IPdfService
-    {
-        byte[] GeneratePdfFromHtml(string htmlContent, dynamic? contratoData = null);
+    {         byte[] GeneratePdfFromHtml(string htmlContent);
+
     }
 }
